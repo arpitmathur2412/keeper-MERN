5b5cd56ad49191d1e0ad7d199e92e4c1408f1b97
Arpit Mathur <98715435+arpitmathur2412@users.noreply.github.com>
2023-03-27 14:57:22 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom"
 
 function Navigationbar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" expand="lg">
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
